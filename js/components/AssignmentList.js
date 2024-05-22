7d3038b8b0cad9743933cfefc8099e8bd9bb9a8b
yopaz-hieuvm <hieuvm1210@gmail.com>
2024-05-22 13:43:20 +0700

    More Flexible Components

--- js/components/AssignmentList.js
+++ js/components/AssignmentList.js
@@ -4,22 +4,20 @@ export default {
     components: { Assignment },
 
     template: `
-        <section v-show="assignments.length">
-            <h2 class="font-bold mb-2">
-                {{ title }}
-                <span>({{ assignments.length }})</span>
-            </h2>
-            
-            <div class="flex gap-2">
-                <button
-                    @click="currentTag = tag"
-                    v-for="tag in tags" 
-                    class="border rounded px-1 py-px text-xs"
-                    :class="{
-                        'border-blue-500 text-blue-500': tag === currentTag 
-                    }"
-                >{{ tag }}</button> 
+        <section v-show="assignments.length" class="w-60">
+            <div class="flex justify-between items-start">
+                <h2 class="font-bold mb-2">
+                    {{ title }}
+                    <span>({{ assignments.length }})</span>
+                </h2>
+                
+                <button v-show="canToggle" @click="$emit('toggle')">&times;</button>
             </div>
+            
+            <assignment-tags
+                v-model:currentTag="currentTag"
+                :initial-tags="assignments.map(a => a.tag)" 
+            ></assignment-tags>
             <ul class="border border-gray-600 divide-y divide-gray-600 mt-6">
                <assignment 
                     v-for="assignment in filteredAssignments"
@@ -27,17 +25,20 @@ export default {
                     :assignment="assignment"
                 ></assignment>
             </ul>
+            
+            <slot></slot>
         </section> 
     `,
 
     props: {
         assignments: Array,
-        title: String
+        title: String,
+        canToggle: { type: Boolean, default: false }
     },
 
     data() {
         return {
-            currentTag: 'all'
+            currentTag: 'all',
         };
     },
 
@@ -48,10 +49,6 @@ export default {
             }
 
             return this.assignments.filter(a => a.tag === this.currentTag);
-        },
-
-        tags() {
-            return ['all', ...new Set(this.assignments.map(a => a.tag))];
         }
     }
 }
